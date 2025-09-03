6c7c43035d1010bd71c8e1b50c4dd00478175d8d
Anna Grigoryan <a.grigoryananna001@gmail.com>
2025-09-03 21:24:06 +0400

    FirstCommit

---
+++ Anna Grigoryan.py
@@ -0,0 +1,11 @@
+print("it's my first program!")
+
+print("Anna Grigoryan", end = 3*"\n")
+
+print("""I have worked as IT information security officer at SoftConstruct.
+I have worked as a QA engineer at Neetry.
+I am working as a QA engineer at ApexWave.""")
+
+
+sum = 7+5
+print(sum)
