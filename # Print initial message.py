e759241496f1150d09ffdb586ab337a4a3d71b35
hamletbalasanyan <hamlet.balasanyan@gmail.com>
2025-09-03 19:15:47 +0400

    Second Lesson: First Program

---
+++ # Print initial message.py
@@ -0,0 +1,18 @@
+# Print initial message
+print("It's my first program", end="!\n")
+
+# Personal information
+name = "Hamlet"
+surname = "Balasanyan"
+print(name + " " + surname, "\n")
+
+# Backround information
+print("I'm Hamlet Balasanyan, from Goris." "\n"
+      "I work as a QA engineer at Digitain Armenia." "\n"
+      "Apart from my job, I enjoy watching and playing football." "\n"
+      "My favorite football club is Liverpool")
+
+# Simple arithmetic operation
+a, b = 11, 15
+sum = a + b
+print(sum)
