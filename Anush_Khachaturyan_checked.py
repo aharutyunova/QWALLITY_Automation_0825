b75892658ec3b179b3eb700da8571f6195f32e8e
AnushKhachaturyan <anushkh1989@gmail.com>
2025-09-03 19:56:01 +0400

    First file upload

---
+++ Anush_Khachaturyan_checked.py
@@ -0,0 +1,11 @@
+print("It's my first program!")
+print("Anush Khachaturyan", end=2*"\n")
+background = '''I graduated from ASUE.
+I worked in a bank.
+I changed my carrer path.
+I am QA engineer.'''
+print(background)
+sum = 2+8
+print(sum)
+
+# TODO Very good job, everything is correct
