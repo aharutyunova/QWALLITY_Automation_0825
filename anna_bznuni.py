cda74d7c809c37e3ba04c9a9729d03292a313328
Anna-Bznuni <annabznuni4@gmail.com>
2025-09-04 18:58:45 +0400

    Edited homework

---
+++ anna_bznuni.py
@@ -0,0 +1,8 @@
+print("It's my first program", end="!\n")
+print("Anna Bznuni", end="\n\n")
+print("""I am a manual Quality Assurance engineer. I am working at AOByte company and I have more than 4 
+years of experience.""")
+
+a = 1
+b = 4
+print(a + b)
