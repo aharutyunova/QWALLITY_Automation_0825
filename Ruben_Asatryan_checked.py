88209896ffad2b1f29d09e1da9720d72d8ec601f
rubenasatryan <ruben.asatryan95@gmail.com>
2025-09-07 13:45:12 +0400

    homework4

--- Ruben_Asatryan_checked.py
+++
@@ -1,9 +0,0 @@
-print("It's my first program", end="!\n")
-print("Ruben Asatryan", end=2*'\n')
-print("I am Ruben Asatryan, and I work as a QA Engineer"
-      " at Nork Technology Center.")
-a = 2
-b = 3
-print(a + b)
-
-# TODO Good job, all requirements are covered.
