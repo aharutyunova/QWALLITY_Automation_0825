e9a6af17af1e3217b170ed88dd9ac5d66b92d5a8
Victoria_Badalyan <victoria.badalyan9@gmail.com>
2025-09-08 17:20:00 +0400

    Add a new file to the branch

---
+++ Victoria_Badalyan.py
@@ -0,0 +1,8 @@
+print("It is my first programme!")
+print("Victoria Badalyan")
+print()
+print("I have studied QA in QWALLITY and I have been working as a QA engineer"
+      "for about one and a half years now.")
+a = 50
+b = 40
+print(a+b)
