9783f4c1dc2b288f2a614ee72b01682d691df0fc
Hayk Khachatryan <haykone100@gmail.com>
2025-09-08 19:49:23 +0400

    homework

---
+++ lesson4Hayk.py
@@ -0,0 +1,33 @@
+# ex1
+my_string = 'Here is string for your exercise!'
+
+count = len(my_string)
+print(count)
+# ex2
+Sample_String = 'w3resource'
+print(Sample_String[0:2] + Sample_String[8:10])
+# ex3
+ex3_String = 'I have a cat and I love it'
+
+change = ex3_String.replace("cat", "dog")
+print(change)
+# ex4
+ex4_String = 'I have 123 books'
+
+rev = ex4_String.replace("123", "321")
+print(rev)
+# ex5
+ex5_String = "five five was a race horse, two two was one too."
+newreplace = ex5_String.replace("five", "one")
+print(newreplace)
+# ex6
+ex6_string1 = [1, 5, 8, 3]
+print(3 in ex6_string1)
+print(-1 in ex6_string1)
+# ex7
+
+x = 4
+y = 3
+result = (x + y) * (x + y)
+print(result)
+print("s")
