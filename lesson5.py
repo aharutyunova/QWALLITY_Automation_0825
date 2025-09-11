5b70004b5665433b197f0790c44a7ad9c5d67677
Anna <an.harutyunova@gmail.com>
2025-09-11 08:40:13 +0400

    Add Comments Lesson 5

--- lesson5.py
+++ lesson5.py
@@ -5,15 +5,25 @@ values=[1,2,3,4,5,6,7,8,9,9]
 #1 Write a Python program to sum all the items in a list.(write logic)
 print("The sum is list:", sum(values))
 
+# TODO: I expected an implementation of the sum algorithm, not the use of the built-in sum() function.
+
 #2 Write a Python program to remove duplicates from a list.
 uniques = list(set(values))
-print("unique list is:", uniques)  
+print("unique list is:", uniques)
+
+# Anna - Correct
 
 #3 Write a Python program which print a specified list after removing the 0th, 4th and 5th elements.  
 values.pop(0)
 values.pop(4)
 values.pop(5)
 print("removed 0th, 4th,5th elements:", values)
+
+# TODO: When you remove index 0, the remaining indexes shift 
+# (so the original index 4 becomes index 3, etc.). 
+# To avoid this, remove elements starting from the last index: 
+# pop(5), pop(4), and then pop(0). 
+# This way, the original indexes won’t change.
 
 #4 Write a Python program to get the difference between the two lists.
 
@@ -26,11 +36,15 @@ print("In values but not in values2:", diff1)
 print("In values2 but not in values:", diff2)
 print("Difference between lists:", diff_all)
 
+# Anna - Correct
+
 #5 Write a Python program to convert a tuple to a dictionary.
 
 tuple_data = (("a", 1), ("b", 2), ("c", 3))
 result = dict(tuple_data)
 print("Converted dictionary:", result)
+
+# Anna - Correct
 
 #6 Write a Python program to add an item in a tuple.
 
@@ -38,11 +52,14 @@ tuple = (1, 2, 3)
 new_tuple = tuple + (4,)
 print(new_tuple)
 
+# Anna - Correct
 
 #7 Write a Python program to add a key with corresponding value to a dictionary.
 dict = {"a": 1, "b": 2}
 dict["c"] = 3
 print("Updated dictionary:", dict)
+
+# Anna - Correct
 
 #8 Write a Python program to get the maximum and minimum value in a dictionary.
 
@@ -51,6 +68,8 @@ max_value = max(dict.values())
 print("Min in dictionary:", min_value)
 print("Max in dictionary:", max_value)
 
+# Anna - Correct
+
 #9 Write a Python program to create a union of sets.
 
 set1 = {1,2,3}
@@ -58,5 +77,9 @@ set2 = {4,5,6}
 unioned = set1.union(set2)
 print("union set is:", unioned)
 
+# Anna - Correct
+
 #10 Write a Python program to create dict with your name, age, address, education, and two phone numbers. 
 dictMyInfo = {"name": "Anna", "age": 27, "address":"home", "education": "higher" , "phones": [12345,12345]}
+
+# Anna - Correct
