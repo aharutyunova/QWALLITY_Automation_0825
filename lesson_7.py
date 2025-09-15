c62a8301423491bd00ca8153bf496b2aa812901e
Anna <an.harutyunova@gmail.com>
2025-09-15 21:50:09 +0400

    Add Lesson 7 homework

---
+++ lesson_7.py
@@ -0,0 +1,17 @@
+"""
+1.	Write a Python function to find the maximum value of given list (write algorithm).
+
+2.	Write a Python function, which will get few numbers from keyboard, return to sum of them, need to write sum logic.
+Sample List : (2, 1, 4, 0, 7)
+Expected Output : 14
+
+
+3.	Create module where keep function of Fibonacci, in second .py file import and call it.
+
+
+4.	Create function, which takes 2 arguments 
+•  name and surname
+•  In function create dictionary to store your name and surname
+•  Return the created dictionary
+
+"""
