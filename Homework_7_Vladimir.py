ba748a104717e0414975b12ae9fb2825d77cd223
Anna <an.harutyunova@gmail.com>
2025-09-18 00:10:33 +0400

    Add Comments Lesson 7

--- Homework_7_Vladimir.py
+++ Homework_7_Vladimir.py
@@ -3,6 +3,7 @@ from Lesson_7_folder import Fibonacci_file
 
 Fibonacci_file.fib(80)
 
+# Anna - Correct
 
 # 1
 def get_max_value(my_list):
@@ -14,6 +15,10 @@ def get_max_value(my_list):
 
 
 print(get_max_value([1, 8, -8, 78]))
+
+# TODO: The logic is mostly correct, but it's better to set max_value to the first element of the list.
+#       This way, the function works even if all numbers are negative.
+
 
 # 2
 
@@ -28,8 +33,9 @@ def get_sum_of_numbers():
 
 print(get_sum_of_numbers())
 
-# 4
+# Anna - Correct
 
+# 4
 
 def full_name(name, surname):
     my_full_name = {"my_name": name, "my_surname":  surname}
@@ -37,3 +43,6 @@ def full_name(name, surname):
 
 
 print(full_name("Vladimir", "Voskanyan"))
+# Anna - Correct
+
+# Good Job!!!
