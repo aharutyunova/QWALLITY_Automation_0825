621c6d266fdba24755084cfbe66291a4193e643e
Anna-Bznuni <annabznuni4@gmail.com>
2025-09-18 19:59:31 +0400

    edited homework 7

--- Lesson 7/home_work_7.py
+++ Lesson 7/home_work_7.py
@@ -2,17 +2,21 @@
 1.	Write a Python function to find the maximum value of given list (write algorithm).
 """
 
-our_given_list = [1, 5, 43, 4, 70]
+our_given_list = [1, 5, 43, 4, 73]
+
+
 def get_max_value(given_list):
-	max_value = 0
-	for i in range(len(given_list)-1):
-		if given_list[i] > given_list[i+1]:
-			max_value = given_list[i]
-		else:
-			continue
-	return max_value
+    max_value = given_list[0]
+    for i in given_list:
+        if i > max_value:
+            max_value = i
+        else:
+            continue
+    return max_value
+
 
 print(get_max_value(our_given_list))
+
 # TODO: range() excludes the last value, so don't subtract 1 — just use:
 # for i in range(len(given_list)):
 
@@ -22,24 +26,26 @@ Sample List : (2, 1, 4, 0, 7)
 Expected Output : 14
 """
 
+
 def sum_of_numbers(numbers):
-	sum_item = 0
-	for n in numbers:
-		sum_item += n
-	return sum_item
+    sum_item = 0
+    for n in numbers:
+        sum_item += n
+    return sum_item
+
 
 inputted_numbers = input("Enter numbers separated by spaces: ")
 
 list_1 = []
 
 for x in inputted_numbers.split():
-	if x.isdigit():
-		list_1.append(int(x))
-	else:
-		print("Please enter only numbers.")
-		break
+    if x.isdigit():
+        list_1.append(int(x))
+    else:
+        print("Please enter only numbers.")
+        break
 else:
-	print("Sum:", sum_of_numbers(list_1))
+    print("Sum:", sum_of_numbers(list_1))
 
 # Anna - Good solution
 """
@@ -57,9 +63,11 @@ print(Fibonacci_file.fibonacci(0, 1, 50))
 •  Return the created dictionary
 """
 
+
 def name_and_surname(name, surname):
-	n_and_sname = {"name": name, "surname": surname}
-	return n_and_sname
+    n_and_sname = {"name": name, "surname": surname}
+    return n_and_sname
+
 
 print(name_and_surname("Anna", "Bznuni"))
 
