c8d27b9449ce1f113e79d49b7074542bb922a458
AnushKhachaturyan <anushkh1989@gmail.com>
2025-09-18 07:48:06 +0400

    Homework 7

--- lesson_7.py
+++ lesson_7.py
@@ -1,17 +1,50 @@
 """
-1.	Write a Python function to find the maximum value of given list (write algorithm).
+1.	Write a Python function to find the maximum value of given list (write algorithm)."""
 
-2.	Write a Python function, which will get few numbers from keyboard, return to sum of them, need to write sum logic.
+def maxvalue(l):
+    maxvalue = l[0]
+    for i in l:
+        if i > maxvalue:
+            maxvalue = i
+    return maxvalue
+
+l1 = [8, 7, 4, 11]
+print(maxvalue(l1))
+
+"""2.	Write a Python function, which will get few numbers from keyboard, return to sum of them, need to write sum logic.
 Sample List : (2, 1, 4, 0, 7)
-Expected Output : 14
+Expected Output : 14"""
+def sum_num():
+    numbers = input("Enter numbers")
+    l =[]
+    for i in numbers:
+        l.append(int(i))
+    totalsum = 0
+    for j in l:
+        totalsum += j
+    return totalsum
+x = sum_num()
+print(x)
 
+def sum_num2(*args):
+    res = sum(args)
+    return res
 
-3.	Create module where keep function of Fibonacci, in second .py file import and call it.
+print(sum_num2(5, 4))
 
+"""3.	Create module where keep function of Fibonacci, in second .py file import and call it."""
+from Myfunction import Fibonacci
 
-4.	Create function, which takes 2 arguments 
+print(Fibonacci.myfibonacci(50))
+
+"""4.	Create function, which takes 2 arguments 
 •  name and surname
 •  In function create dictionary to store your name and surname
 •  Return the created dictionary
 
 """
+def myData(myname, mysurname):
+    my_dict = {"name": myname, "surname": mysurname}
+    return my_dict
+
+print (myData("Anush", "Khachaturyan"))
